605aaf41091b0218736ba93bf4fbbc980fa6b6b5
jthapata <jessezap01@gmail.com>
2024-04-01 09:51:39 -0600

    testing api

--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -1,29 +1,54 @@
+'use client'
 import { useEffect, useState } from "react";
 
 export default function CryptoList () {
     const [list, setList] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        
-    })
-
-    async function getCryptos() {
-        let url = 'https://api.coincap.io/v2/assets'
-        try {
-            let requestOptions = {
-                method: 'GET',
-                redirect: 'follow',
-                limit: 15
+        setLoading(true)
+        async function getCryptos() {
+            const url = 'https://api.coincap.io/v2/assets'
+            try {
+                const requestOptions = {
+                    method: 'GET',
+                    redirect: 'follow'
+                }
+                const response = await fetch(url, requestOptions)
+                const data = await response.json()
+                const cryptoData = data.data
+                console.log(cryptoData[0])
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setError(error)
             }
-            const response = fetch(url, requestOptions)
-            const data = await response.json()
-            console.log(data)
-        } catch (error) {
-            console.error('Error fetching data:', error);
         }
+        getCryptos()
+        setLoading(false)
+    }, [])
+
+    if (loading) {
+        return (
+            <div>Loading...</div>
+        )
+    }
+    if (error) {
+        return (
+            <div>Error</div>
+        )
     }
 
     return (
-        <div>test</div>
+        <div>
+            test
+            {/* <ul>
+                {list.map((coin) => {
+                    <li key={coin.id}>
+                        {coin.name}
+                    </li>
+                })}
+            </ul> */}
+        </div>
     )
 }
