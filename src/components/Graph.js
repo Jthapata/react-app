24af684cce075920468fa511f24b9eecdb436d16
jthapata <jessezap01@gmail.com>
2024-04-11 21:05:30 -0600

    ruined the graph by trying to use material ui

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,18 +1,22 @@
-import {
-    LineChart,
-    Line,
-    XAxis,
-    YAxis,
-    Tooltip
-  } from "recharts";
-  
-export default function Graph( props ) {
+import * as React from 'react';
+import { LineChart} from '@mui/x-charts';
+
+export default function Graph(props) {
+    console.log(props.id)
     return (
-        <LineChart width={800} height={300} data={props.data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+        <LineChart
+            width={500}
+            height={300}
+            xAxis={{ dataKey: 'time', title: 'Time' }} // Define x-axis with label
+            yAxis={{ dataKey: 'priceUsd', title: 'Price' }} // Add y-axis label (replace with your desired label)
+            dataset={[...props.id]}
+        />
+    );
+}
+
+{/* <LineChart width={800} height={300} data={props.data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
             <XAxis dataKey="time" stroke="white" />
             <YAxis stroke="white" domain={['auto', 'auto']} width={100}/>
             <Tooltip />
             <Line type="monotone" dataKey="priceUsd" stroke="#EF4444" activeDot={{ r: 8 }} />
-        </LineChart>
-    );
-}
+        </LineChart> */}
