d91af385f8f521d3a83aa16f82f00ebbb3e0b845
jthapata <jessezap01@gmail.com>
2024-04-26 08:22:35 -0600

    fixing header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 export default function Header() {
     return (
         <div className="bg-background p-4 pl-9 flex flex-col">
-            <Link href="/" >
+            <Link href="/" className="w-fit">
                 <h1 className="bg-gradient-to-r from-primary to-accent text-6xl inline-block text-transparent bg-clip-text p-3">CryptOhs</h1>
             </Link>
             <hr className='my-4' />
